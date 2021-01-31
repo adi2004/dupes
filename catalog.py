39f4b1687b4f23d07cdc27450a56a3af35a67094
Adrian Florescu <adrian.florescu@softvision.ro>
2021-01-31 06:26:56 +0200

    added flags for command line, improved view

--- catalog.py
+++ catalog.py
@@ -14,18 +14,23 @@ import dhash
 import imagehash
 from PIL import Image
 
-if len(sys.argv) != 3:
-    print("Use ./catalog.py <dir-to-catalog> <catalog-file.csv")
-    exit(1)
-
-exclude_list = [".git", "@eaDir", "backup", "incoming"]
-include_list = ['.mov', '.avi', '.mp4', '.m4v', '.wmv', '.mpg', '.mpeg']
-
 
 class Const:
     gibi = 1024 * 1024 * 1024
     mebi = 1024 * 1024
     kibi = 1024
+    progress = ["|", "/", "-", "|", "\\"]
+
+    md5_hash_flag = "5"
+    exiftool_flag = "e"
+    avarage_hash_flag = "a"
+    difference_hash_flag = "d"
+
+class View:
+    file_count = 0
+    total_size = 0
+    printed_length = 0
+
 
 def is_ignored(filename):
     for exclude_keyword in exclude_list:
@@ -45,7 +50,27 @@ def remove_prefix(text, prefix):
 
 
 def dt(timestamp):
-    return datetime.fromtimestamp(timestamp).strftime("%Y.%m.%d %H.%M.%S")
+    return datetime.fromtimestamp(timestamp).strftime("%Y.%m.%d %H.%M")
+
+
+def print_progress(file_path, file_name, view):
+    if view.total_size >= Const.gibi:
+        size_string = "%.2f GiB" % (view.total_size / Const.gibi)
+    elif view.total_size >= Const.mebi:
+        size_string = "%.2f MiB" % (view.total_size / Const.mebi)
+    else:
+        size_string = "%.2f KiB" % (view.total_size / Const.kibi)
+
+    spinner = " " + Const.progress[view.file_count % 5] + " "
+    out_string = "\rFound {:,d}".format(view.file_count) + " files" + \
+        "totaling " + size_string + spinner + \
+        "reading " + file_path + "/" + file_name
+    if len(out_string) > 120:
+        out_string = out_string[:100] + "..." + out_string[-20:]
+    end_padding = view.printed_length - len(out_string)
+    end_string = " " * (end_padding if end_padding > 0 else 0)
+    print(out_string + end_string, end="")
+    return len(out_string)
 
 
 def md5_hash(filename):
@@ -115,40 +140,46 @@ def write(file, catalog):
         writer.writerows(catalog)
 
 
+if len(sys.argv) != 4:
+    print("Use ./catalog.py flags <dir-to-catalog> <catalog-file.csv")
+    print(
+        "The flags are:\n" +
+        Const.md5_hash_flag + " for MD5" +
+        Const.exiftool_flag + " for MD5" +
+        Const.avarage_hash_flag + " for aHash algorithm" +
+        Const.difference_hash_flag + " for dHash algorithm (recommended)"
+    )
+    exit(1)
+
+exclude_list = [".git", "@eaDir", "backup", "incoming"]
+include_list = ['.mov', '.avi', '.mp4', '.m4v', '.wmv', '.mpg', '.mpeg']
+
+
 start_time = time.time()
-catalog_directory = sys.argv[1]
-catalog_file_name = sys.argv[2]
+flags = sys.argv[1].split()
+catalog_directory = sys.argv[2]
+catalog_file_name = sys.argv[3]
 print("Reading {}/{}".format(os.getcwd(), catalog_directory))
 
 catalog_paths = list(Path(catalog_directory).rglob("*"))
 
 catalog = []
+view= View()
 
-file_count = 0
-total_size = 0
-progress = ["|", "/", "-", "|", "\\"]
 for path in catalog_paths:
     path_parent = str(path.parent)
     file_name = str(path.name)
     file_with_path = path_parent + "/" + file_name
+    path_parent_short = remove_prefix(path_parent, catalog_directory)
+    path_parent_short = re.sub("^[./]+", "", path_parent_short)
     if path.is_dir() or is_ignored(file_with_path) or not is_included(file_with_path):
         continue
-    file_count += 1
+    view.file_count += 1
     fprop = {}
     try:
         info = path.stat()
         # populate filter criteria
-        path_parent = remove_prefix(path_parent, catalog_directory)
-        path_parent = re.sub("^[./]+", "", path_parent)
-        if total_size >= Const.gibi:
-            size_string = "%.2f GiB" % (total_size / Const.gibi)
-        elif total_size >= Const.mebi:
-            size_string = "%.2f MiB" % (total_size / Const.mebi)
-        else:
-            size_string = "%.2f KiB" % (total_size / Const.kibi)
-        print("Found {} files totaling {} {} reading {}"
-            .format(file_count, size_string, progress[file_count % 5], file_with_path[0:120] + " " * 60), 
-            end="\r")
+        view.printed_length = print_progress(path_parent_short, file_name, view)
         paths = path_parent.split("/")
         fprop["path_part_1"] = paths[0] if len(paths) > 0 else ""
         fprop["path_part_2"] = paths[1] if len(paths) > 1 else ""
@@ -164,27 +195,31 @@ for path in catalog_paths:
         fprop["modified_time"] = dt(info.st_mtime)
         fprop["created_time"] = dt(info.st_ctime)
         fprop["access_time"] = dt(info.st_atime)
-
         # size
         fprop["file_size"] = info.st_size
-        total_size += info.st_size
+        view.total_size += info.st_size
+
         # md5 hash
-        fprop["md5_hash"] = md5_hash(file_with_path)
+        if Const.md5_hash_flag in flags:
+            fprop["md5_hash"] = md5_hash(file_with_path)
         # get date using exiftool
-        fprop["exiftool_date"] = exiftool_date(file_with_path)
-        # get muvie duration using exiftool
-        fprop["exiftool_duration"] = exiftool_duration(file_with_path)
+        if Const.exiftool_flag in flags:
+            fprop["exiftool_date"] = exiftool_date(file_with_path)
+            fprop["exiftool_duration"] = exiftool_duration(file_with_path)
         # image average hash
-        fprop["image_average_hash"] = image_average_hash(file_with_path)
+        if Const.avarage_hash_flag in flags:
+            fprop["image_average_hash"] = image_average_hash(file_with_path)
         # image_difference_hash
-        fprop["image_difference_hash"] = image_difference_hash(file_with_path)
+        if Const.difference_hash_flag in flags:
+            fprop["image_difference_hash"] = image_difference_hash(
+                file_with_path)
 
         # add to catalog
         catalog.append(fprop)
     except KeyboardInterrupt:
         exit(1)
     except:
-        print("\nGot it!", sys.exc_info()[0], "occurred.\n")
+        print("\nError ", sys.exc_info()[0], "occurred.")
         continue
 
 print("\n")
